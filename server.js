af0e60cc95f03b7a355ee02c643f0ef9e1d75ff9
Dorota Tomala <doforever@gmail.com>
2021-04-14 14:34:16 +0200

    Render hello with handlebars

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -26,6 +29,10 @@ app.get('/home', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.show('about.html');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
